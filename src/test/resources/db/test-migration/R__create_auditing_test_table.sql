9e69ad1109827fe9ea5798a1472d0c0d9e7134c7
Ayush-Ag11 <ayushagnihotri22@gmail.com>
2026-07-15 13:39:29 +0530

    fix: correct Hibernate default schema configuration

--- src/test/resources/db/test-migration/R__create_auditing_test_table.sql
+++ src/test/resources/db/test-migration/R__create_auditing_test_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS shopverse.auditing_test_entity
     name       VARCHAR(100)             NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
-                             version    BIGINT                   NOT NULL,
+    version    BIGINT                   NOT NULL,
 
-                             CONSTRAINT pk_auditing_test_entity PRIMARY KEY (id)
+    CONSTRAINT pk_auditing_test_entity PRIMARY KEY (id)
     );
